d704c78d0c0da9b4eb76fc40cefa6554e9cbd632
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-31 06:53:25 +0300

    countryList.forEach ((item, index) => {

--- app.js
+++ app.js
@@ -17,3 +17,11 @@ let selectedCountry;
 let selectedCountryCode;
 const countrySelectorButton = document.getElementsById('country-selector-button');
 const countryList = document.querySelectorAll('.country-list');
+countryList.forEach ((item, index) => {
+        item.addEventListener('click', ()=>{
+            selectedCountry 
+        })
+
+}
+
+)
